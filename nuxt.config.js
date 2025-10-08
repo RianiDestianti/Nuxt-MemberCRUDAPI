2356d334189560d8102cf274f5b3b721ddce94c6
Riani Destianti <rianidestiantii@gmail.com>
2025-10-08 21:10:41 +0700

    refactor: clean up nuxt.config.js by removing unused CSS imports and organizing link/script tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,16 +9,35 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { 
+        rel: 'stylesheet', 
+        href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' 
+      },
+      { 
+        rel: 'stylesheet', 
+        href: '/assets/style.css' 
+      },
+      { 
+        rel: 'stylesheet', 
+        href: '/assets/main.css' 
+      }
+    ],
+    script: [
+      {
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
+        defer: true
+      },
+      {
+        src: 'https://cdn.tailwindcss.com',
+        defer: true
+      }
     ]
   },
 
   loading: { color: '#fff' },
 
-  css: [
-    '~/static/assets/style.css',
-    '~/static/assets/main.css'
-  ],
+  css: [],
 
   plugins: [
     '~/plugins/api.js',
